fddd3382f764ec25cf09865691541240d639bfa8
panbi7 <panbi7@naver.com>
2026-01-14 12:03:16 +0900

    edit : function

--- netlify/functions/utils/constants.js
+++ netlify/functions/utils/constants.js
@@ -1,9 +1,5 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import dotenv from 'dotenv'
-
-// Load .env file
-dotenv.config({ path: path.join(path.dirname(fileURLToPath(import.meta.url)), '..', '.env') })
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
